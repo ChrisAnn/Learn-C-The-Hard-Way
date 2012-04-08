a75a2fcb9f56219eb8c1fb9bf313e93992c79701
Chris <Christine.ann.odell@gmail.com>
2012-04-08 19:14:45 +0100

    Added a directory for exercise 2 and the makefile from the exercise.

---
+++ ex2/ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    puts("Hello world.");
+    puts("The sky above the port was the color of television, tuned to a dead channel.");
+    puts("I don't want to waste time looking for 5 more lines of text");
+    puts("I find that I'm easily distracted by 'easier' tasks in order to avoid the 'tougher' ones");
+    puts("So, instead, I hope that this rambling will suffice.");
+    
+    return 0;
+}
